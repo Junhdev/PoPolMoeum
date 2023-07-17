e38e7c4bdd1a05f54b5461d7fef70772d3029384
이준형 <dlwnsgud261@naver.com>
2023-07-17 22:50:12 +0900

    feat: User Entity 생성

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,20 +1,57 @@
-/*
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import BaseEntity from "./Entity";
+import { IsEmail, Length } from "class-validator";
+import { Entity, Column, Index, OneToMany, BeforeInsert } from "typeorm";
+import bcrypt from 'bcryptjs';
+import { Expose } from "class-transformer";
 
-@Entity()
-export class User {
+@Entity("users")
+export class User extends BaseEntity {
+    @Index()
+    @Length(6, 20 , {message: "사용자 아이디는 6자 이상이어야 합니다"})
+    @Column({ unique: true, nullable: true } )
+    userID: string;
 
-    @PrimaryGeneratedColumn()
-    id: number
+    @Index()
+    @IsEmail(undefined, {message: "이메일 주소가 잘못되었습니다."})
+    @Length(1, 255, {message: "이메일 주소는 비워둘 수 없습니다."})
+    @Column({ unique: true })
+    email: string;
+
+    @Index()
+    @Length(3, 32, {message: "사용자 이름은 3자 이상이어야 합니다"})
+    @Column({ unique: true })
+    username: string;
 
     @Column()
-    firstName: string
+    @Length(6, 255, {message: "비밀번호는 6자리 이상이어야 합니다."})
+    password: string;
 
-    @Column()
-    lastName: string
+    @Column({ nullable: true })
+    profileImg: string;
 
-    @Column()
-    age: number
+    
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[];
+
+    
+    @OneToMany(() => Like, (like) => like.user)
+    likes: Like[];
+
+   
+    @OneToMany(() => FriendShip, (friendship) => friendship.user_id)
+    friendship: FriendShip[];
+
+    @OneToMany(() => Crew, (crew) => crew.user_id)
+    friendship: Crew[];
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
+
+    @Expose()
+    get imageUrl(): string {
+        return this.profileImg ? `${process.env.APP_URL}/images/${this.profileImg}` : "https://www.gravatar.com/avatar?d=mp&f=y"; // false === 기본 image
+    }
 
 }
-*/
