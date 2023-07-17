ca2d0916d9ec70cef0ff21b8a2c064f6a733f23e
이준형 <dlwnsgud261@naver.com>
2023-07-17 22:31:42 +0900

    feat: BaseEntity 생성

---
+++ server/src/entities/Entity.ts
@@ -0,0 +1,19 @@
+import { instanceToPlain } from "class-transformer";
+import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export default abstract class Entity extends BaseEntity {
+    /* id열을 Board 엔티티의 기본 키 열로 설정 (인덱스) */
+    @PrimaryGeneratedColumn()
+    id: number; 
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    // 프론트에서 imageUrl과 bannerUrl을 가져올 수 있게 함
+    toJSON() {
+        return instanceToPlain(this);
+      }
+}
