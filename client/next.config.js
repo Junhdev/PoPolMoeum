b69439b4ff08b3d17e68f71f8673b49b8e1fa6cf
이준형 <dlwnsgud261@naver.com>
2023-07-18 23:58:08 +0900

    feat: config 파일 수정

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    reactStrictMode: true, 
+    //swcMinify: true,
+    images: {
+      // 이미지 업로드 관련 domain 설정
+      domains: [
+        "www.gravatar.com", "localhost"
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
+  
