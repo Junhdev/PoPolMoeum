eafe613ea0e44bc8151b110bf06bc2857c9376bd
이준형 <dlwnsgud261@naver.com>
2023-07-27 23:14:28 +0900

    Feat: Button 컴포넌트 속성 추가

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -5,15 +5,17 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children?: ReactNode;
     className?: string;
+    onClick?: () => void;
 } 
 
 const Button = ({   
     children,
     className,
+    onClick,
     ...rest
 }: ButtonProps) => {
     return (
-        <button {...rest}>
+        <button className={className} onClick={onClick} {...rest}>
             <div>{children}</div>
         </button>
     );
