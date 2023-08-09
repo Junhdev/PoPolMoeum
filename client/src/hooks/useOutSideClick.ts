e60ef9cb789cb91b8c181f5d5ceba9a1d2a0c425
이준형 <dlwnsgud261@naver.com>
2023-08-09 23:58:47 +0900

    Feat: useOutSideClick 훅 생성

---
+++ client/src/hooks/useOutSideClick.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+const useOutsideClick = (
+  ref: React.RefObject<HTMLDivElement>,
+  setFocus: (value: boolean) => void
+) => {
+  useEffect(() => {
+    function handleOutsideClick(event: React.BaseSyntheticEvent | MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setFocus(false);
+      }
+    }
+    document.addEventListener("click", handleOutsideClick, true);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick, true);
+    };
+  }, [ref]);
+};
+
+export default useOutsideClick;
