16f470a7f51186c6f278f703d49bf10431650183
이준형 <dlwnsgud261@naver.com>
2023-08-22 23:08:18 +0900

    Feat: 재사용 가능한 ListItem 컴포넌트 생성

---
+++ client/src/components/ListItem/ListItem.tsx
@@ -0,0 +1,63 @@
+import React, { forwardRef } from 'react'
+import { LiHTMLAttributes } from 'react'
+import classNames from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+interface ThumbnailProps {
+  src?: string
+  alt?: string
+  href?: string
+}
+
+interface Props extends LiHTMLAttributes<HTMLLIElement> {
+  isPreviousData?: boolean
+  thumbnailProps?: ThumbnailProps
+}
+
+export const ListItem = ({
+  children,
+  isPreviousData,
+  thumbnailProps,
+  ...props
+}: Props) => (
+  <li
+    {...props}
+    className={classNames('flex flex-col', { 'animate-pulse': isPreviousData })}
+  >
+    {thumbnailProps?.src && (
+      <Link href={thumbnailProps.href || ''} passHref>
+        <Thumbnail {...thumbnailProps} />
+      </Link>
+    )}
+    {children}
+  </li>
+)
+
+const Thumbnail = forwardRef<HTMLDivElement, ThumbnailProps>(
+  ({ src = '', alt = '', href }, ref) => {
+    const router = useRouter()
+    const handlePush = () => href && router.push(href)
+    return (
+      <div
+        className="relative h-40 w-full shrink-0"
+        ref={ref}
+        onClick={handlePush}
+      >
+        {src && (
+          <Image
+            src={src}
+            alt={alt}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-t-lg"
+          />
+        )}
+      </div>
+    )
+  },
+)
+
+ListItem.displayName = 'listItem'
+Thumbnail.displayName = 'thumbnail'
