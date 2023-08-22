6319121418bb5df80e8782b94d0d214b72e0a6e6
이준형 <dlwnsgud261@naver.com>
2023-08-22 21:59:44 +0900

    Feat: atom의 변수명들 수정

--- client/src/store/keyword.ts
+++ client/src/store/keyword.ts
@@ -1,47 +1,52 @@
-
 import { atom, selector } from "recoil";
 
 export interface CareerValue {
   careerId: number;
   careerName: string;
-  totalCareerNum: number;
+  //totalCareerNum: number;
 }
 
 export interface KeywordValue {
   universityId: number;
   universityName: string;
-  totalUniversityNum: number;
+  //totalUniversityNum: number;
   careers: CareerValue[];
 }
 
 export const keywordListState = atom<KeywordValue[]>({
   key: 'keywordState',
-  default: [],
+  default: [{universityId: 1, universityName: '광운대학교', careers:[{careerId: 1, careerName: '개발'}, {careerId: 2,careerName:'디자인'}]},
+  {universityId: 2, universityName: '숭실대학교', careers:[{careerId: 3, careerName: '마케팅'}, {careerId: 4,careerName:'인사'}]}],
 }); 
 
-export const universityListState = atom({
+export const selectedUniversityState = atom({
     key: 'universityState',
-    default: 1,
+    default: 0
 }); 
 
-export const careerListState = atom({
-    key: 'careerState',
-    default: 1,
+export const selectedCareerState = atom({
+    key: 'selectedCareerState',
+    default: 0,
 }); 
   
 export const keywordsFilterState = selector({
     key: 'keywordsFilterState',
     get: ({ get }) => {
       const keywords = get(keywordListState);
-      const selectedUniversityId = get(universityListState);
-      const selectedCareerId = get(careerListState);
+      const selectedUniversityId = get(selectedUniversityState);
+      const selectedCareerId = get(selectedCareerState);
       // selectedUniversity의 type은 Object
-      const selectedUniversity = selectedUniversityId ? keywords.find((keyword) => keyword.universityId === selectedUniversityId) : undefined;
-      const selectedCareer = selectedUniversity?.careers.find((career) => career.careerId === selectedCareerId);
-     
+      const selectedUniversityData = selectedUniversityId ? keywords.find((keyword) => keyword.universityId === selectedUniversityId) : undefined;
+      const selectedCareerData = selectedUniversityData?.careers.find((career) => career.careerId === selectedCareerId);
+      
       return {
-        selectedUniversity,
-        selectedCareer
+        selectedUniversityData,
+        selectedCareerData
       };
     },
 }); 
+
+// DropdownMenu 원리 context 원리
+// 스터디 생성할때 atom에 업데이트해주기(추가 > 신문법 이용) > 그러면 atom[]에 [{},{}] 식으로 저장됨
+// 데이터베이스에 저장이 아닌 클라이언트에 저장?
+// [] [{},{..[{},{}]}] {} 타입중요!
