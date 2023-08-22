f1b4d32d1f5da7780185a3c9ce28f1e8283d52a2
이준형 <dlwnsgud261@naver.com>
2023-08-22 21:57:00 +0900

    Feat: twin-macro와 styled-components를 이용하여 스타일 컴포넌트 생성

---
+++ client/src/components/Modal/style.tsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import classNames from 'classnames'
+import { Button } from '../Button/Button'
+
+export interface LayoutProps {
+    children?: React.ReactNode
+  }
+
+export const Modal = ({ children }: LayoutProps) => {
+  return <div>{children}</div>
+}
+
+const ModalBackground = styled.div`
+  ${tw`fixed inset-0 z-[21] px-4 flex items-center justify-center overflow-y-hidden overflow-x-hidden outline-none focus:outline-none`}
+`
+export interface ContentSize {
+  size?: 'small' | 'medium' | 'large'
+}
+const ModalContainer = styled.div<ContentSize>`
+  ${tw`relative my-8  bg-white rounded-2xl p-6`}
+  ${(props) =>
+    props.size === 'small'
+      ? tw`w-full sm:max-w-[50%] lg:max-w-[30%]`
+      : props.size === 'large'
+      ? tw`mx-auto w-full max-w-screen-xl`
+      : tw`mx-auto w-full max-w-screen-md`}
+`
+const ModalContent = styled.div`
+  ${tw`relative flex w-full flex-col rounded-lg border-0 bg-white shadow-xl outline-none focus:outline-none`}
+`
+const ModalHeader = styled.div`
+  ${tw`border-gray-200`}
+`
+const ModalTitle = styled.h3`
+  ${tw`text-lg font-medium leading-6 text-gray-900`}
+`
+const ModalBody = styled.div`
+  ${tw`relative flex-auto text-sm text-gray-600 overflow-y-auto`}
+`
+const ModalFooter = styled.div`
+  ${tw`flex items-center justify-end rounded-b pt-3`}
+`
+const ModalBackArea = styled.div`
+  ${tw`fixed inset-0 z-[20] bg-black opacity-25`}
+`
+const ModalCloseButton = styled.button`
+  ${tw`absolute right-6 top-6 text-gray-400 z-[29]`}
+`
+interface CloseButtonProps {
+  className?: string
+  name?: string
+  onClick: () => void
+}
+
+const CloseButton = ({ className, name, onClick }: CloseButtonProps) => {
+  return (
+    <Button
+      className={classNames(
+        className,
+        'absolute right-6 top-6 z-[29] text-gray-400',
+      )}
+      name={name}
+      onClick={onClick}
+    />
+  )
+}
+
+Modal.Background = ModalBackground
+Modal.Container = ModalContainer
+Modal.Content = ModalContent
+Modal.Header = ModalHeader
+Modal.Title = ModalTitle
+Modal.Body = ModalBody
+Modal.Footer = ModalFooter
+Modal.BackArea = ModalBackArea
+Modal.CloseButton = CloseButton
