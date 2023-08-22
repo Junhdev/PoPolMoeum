856ad54802ef3c6b6b5eceb9cef3c48f0acf52de
이준형 <dlwnsgud261@naver.com>
2023-08-22 21:55:36 +0900

    Feat: 기본 모달 컴포넌트 생성

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Portal, defaultModalRootId } from './portal'
+import { ContentSize, Modal as StyledModal } from './style'
+
+export type ModalProps = {
+  isOpen: boolean
+  children?: React.ReactChild
+  handleClose: Function
+  title?: string
+} & ContentSize
+
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  children,
+  handleClose,
+  title,
+  size = 'medium',
+}) => {
+  if (!isOpen) return null
+
+  return (
+    <Portal rootId={defaultModalRootId}>
+      <StyledModal>
+        <StyledModal.Background role="modal">
+          <StyledModal.Container size={size}>
+            <StyledModal.Header>
+              {title && <StyledModal.Title>{title}</StyledModal.Title>}
+              <StyledModal.CloseButton
+                name="close"
+                onClick={() => handleClose()}
+              />
+            </StyledModal.Header>
+            <StyledModal.Body>{children}</StyledModal.Body>
+          </StyledModal.Container>
+        </StyledModal.Background>
+        <StyledModal.BackArea />
+      </StyledModal>
+    </Portal>
+  )
+}
