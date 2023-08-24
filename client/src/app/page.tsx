5e8cd9912815aa42a58d8702543b8cb44d31e7ea
이준형 <dlwnsgud261@naver.com>
2023-08-24 23:58:47 +0900

    Feat: 메인페이지 컴포넌트 구성

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      <h1 className='font-bold text-gray-400 text-small'>Home Page</h1>
-      
+      <h1 className="font-bold text-gray-400 text-small">Home Page</h1>
     </main>
   )
 }
