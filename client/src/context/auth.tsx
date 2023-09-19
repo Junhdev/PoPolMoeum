0f6b36a7af986ac5d9ccfa3b6e14c73e48774e46
이준형 <dlwnsgud261@naver.com>
2023-09-19 17:36:05 +0900

    Feat: 로그인 관련 전역 context 생성

---
+++ client/src/context/auth.tsx
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useReducer } from "react";
+//import { User } from "../types";
+
+export interface User {
+    id: number;
+    user_id: string;
+    username: string;
+    password: string;
+    email: string;
+    universityId: number;
+    // 학년 및 휴학 및 졸업
+    gradeId: number;
+    majorId: number;
+    careerId: number;
+    profileImg: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+
+interface State {
+    authenticated: boolean;
+    user: User | undefined;
+    loading: boolean;
+}
+
+const StateContext = createContext<State>({
+    // 초기값 설정
+    authenticated: false,
+    user: undefined,
+    loading: true
+});
+
+// 업데이트시 이용
+const DispatchContext = createContext<any>(null);
+
+interface Action {
+    type: string;
+    payload: any;
+}
+
+const reducer = (state: State, { type, payload }: Action) => {
+    switch (type) {
+        case "LOGIN":
+            return {
+                ...state,
+                authenticated: true,
+                user: payload
+            }
+        case "LOGOUT":
+            return {
+                ...state,
+                authenticated: false,
+                user: null
+            }
+        case "STOP_LOADING":
+            return {
+                ...state,
+                loading: false
+            }
+        default:
+            throw new Error(`Unknown action type: ${type}`)
+    }
+}
+
+// _app.tsx에서 <Component> 감싸주어야함
+export const AuthProvider = ({children}: { children: React.ReactNode }) => {
+
+    const [state, defaultDispatch] = useReducer(reducer, {
+        user: null,
+        authenticated: false,
+        loading: true
+    })
+
+    console.log('state', state);
+
+    // 유저 정보 업데이트 시 이용
+    const dispatch = (type: string, payload?: any) => {
+        defaultDispatch({ type, payload });
+    }
+
+    // 로그인 후에 로그인 페이지나 회원가입 페이지 접근 시 다시 메인 페이지로 redirect하게 만들기
+    useEffect(() => {
+        async function loadUser() {
+            try {
+                const res = await axios.get("/auth/me");
+                dispatch("LOGIN", res.data);
+            } catch (error) {
+                console.log(error)
+            } finally {
+                dispatch("STOP_LOADING");
+            }
+        }
+        loadUser();
+    }, [])
+
+
+    return (
+        <DispatchContext.Provider value={dispatch}>
+            <StateContext.Provider value={state}>{children}</StateContext.Provider>        
+        </DispatchContext.Provider>
+    )
+}
+
+export const useAuthState = () => useContext(StateContext);
+export const useAuthDispatch = () => useContext(DispatchContext);
