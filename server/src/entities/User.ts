f3368bea91e76adc7dc43efc505444e3cac10ee5
이준형 <dlwnsgud261@naver.com>
2023-09-19 17:38:09 +0900

    Feat: 유저 엔티티 수정

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -3,25 +3,31 @@ import { IsEmail, Length } from "class-validator";
 import { Entity, Column, Index, OneToMany, BeforeInsert } from "typeorm";
 import bcrypt from 'bcryptjs';
 import { Expose } from "class-transformer";
-import Work from "./Work";
+import Work from "./Task";
 import Like from "./Like";
+import Membership from "./Membership";
 
+type Subject = {
+    id:
+    name:
+    
+}
 @Entity("users")
 export class User extends BaseEntity {
     @Index()
     @Length(6, 20 , {message: "사용자 아이디는 6자 이상이어야 합니다"})
     @Column({ unique: true, nullable: true } )
-    userID: string;
+    user_id: string;
 
     @Index()
     @IsEmail(undefined, {message: "이메일 주소가 잘못되었습니다."})
     @Length(1, 255, {message: "이메일 주소는 비워둘 수 없습니다."})
-    @Column({ unique: true })
+    @Column({ unique: true, nullable: true})
     email: string;
 
     @Index()
     @Length(3, 32, {message: "사용자 이름은 3자 이상이어야 합니다"})
-    @Column({ unique: true })
+    @Column({ unique: true , nullable: true})
     username: string;
 
     @Column()
@@ -29,23 +35,23 @@ export class User extends BaseEntity {
     password: string;
 
     @Column({ nullable: true })
+    universityId: number;
+
+    @Column({ nullable: true })
+    majorId: number;
+
+    // 회원가입할때 선택 -> recoil 이용
+    @Column({ nullable: true })
+    interstingSubject: Subject[];
+
+    @Column({ nullable: true })
     profileImg: string;
 
-    
     @OneToMany(() => Work, (work) => work.user)
     works: Work[];
 
-    
     @OneToMany(() => Like, (like) => like.user)
     likes: Like[];
-
-   
-    @OneToMany(() => FriendShip, (friendship) => friendship.user_id)
-    friendship: FriendShip[];
-
-    @OneToMany(() => Crew, (crew) => crew.user_id)
-    friendship: Crew[];
-    
 
     @BeforeInsert()
     async hashPassword() {
