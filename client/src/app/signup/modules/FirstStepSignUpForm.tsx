739c8a81d1e6dcbd6b148e14c0d3904d0480e3af
이준형 <dlwnsgud261@naver.com>
2023-10-02 23:46:09 +0900

    Feat: Context에서 가져온 상태 useEffect의 deps에 추가하여 폼 제출 논리 수정

---
+++ client/src/app/signup/modules/FirstStepSignUpForm.tsx
@@ -0,0 +1,148 @@
+'use client'
+
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import { useRouter } from 'next/navigation';
+import { useAuthState } from '@/context/auth';
+import { useMultiStepFormContext } from '@/components/Form/context/MultiStepFormContext';
+import { Form } from '@/components/Form/Form';
+import { FormInput } from '@/components/Form/FormInput';
+import { Button } from '@/components/Button/Button';
+import { useEffect, useState } from 'react';
+
+
+export type FirstStepSignUpValue = {
+    username: string;
+    user_id: string;
+    password: string;
+    email: string;
+}
+
+
+
+export const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
+
+// multi step 으로 ( 마이페이지의 관심 카테고리와 연결 !
+// ex) 아이콘으로!! 관심 분야를 선택해주세요.. 추후 이 관심분야를 바탕으로 추천 분야 스터디!)
+const FirstStepSignUpForm = () => {
+ 
+    const router = useRouter();
+    const { authenticated } = useAuthState();
+
+    if (authenticated) router.push("/");
+
+    const { stepFormData, setStepFormData, onHandleNext, } = useMultiStepFormContext();
+    
+  
+  
+
+        const firstStepSignUpSchema = Yup.object().shape({
+            // 추후 대학교 검색 api 생성하여 받아오기
+            // 폼 안에 드롭다운으로 선택 구현
+            // 날짜 위치 선택 구현
+            username: Yup.string()
+            .min(1, '너무 짧습니다')
+            .max(10, '10자 이내로 입력해 주세요')
+            .required('사용자 이름을 입력해 주세요'),
+            user_id: Yup.string()
+            .min(3, '너무 짧습니다')
+            .max(15, '15자 이내로 입력해 주세요')
+            .required('아이디를 입력해 주세요'),
+            password: Yup.string()
+            .min(3, '너무 짧습니다')
+            .max(20, '20자 이내로 입력해 주세요')
+            .required('비밀번호를 입력해 주세요')
+            .matches(
+                passwordReg,
+                '비밀번호는 대문자, 소문자, 숫자 조합으로 입력해주세요',
+              ),
+              /*
+              password2: Yup.string()
+              .required('비밀번호를 확인해주세요')
+              .oneOf([Yup.ref('password1'), null], '비밀번호가 다릅니다.'),
+              */
+            email: Yup.string()
+            .email('이메일 형식에 맞게 입력해주세요')
+            .min(3, '너무 짧습니다')
+            .max(50, '50자 이내로 입력해 주세요')
+            .required('이메일을 입력해주세요'),
+            
+          })
+    
+       
+        
+          useEffect(()=>{
+         
+            console.log(stepFormData);
+            onHandleNext()},
+            [stepFormData])
+
+          const [mounted, setMounted] = useState(false)
+          useEffect(() => {
+            setMounted(true)
+          }, [])
+
+        const handleCreateFirstStep = async (firstStepData: FirstStepSignUpValue) => {
+          
+           
+            setStepFormData((prev: any) => ({ ...firstStepData }));
+            
+          
+           
+            //console.log(stepFormData)
+            //onHandleNext();
+            
+        };
+       
+  
+        return (
+            <div>
+              <div>
+                <p className="mb-4 mt-1 text-sm">
+                  회원가입
+                </p>
+                
+                <Form<FirstStepSignUpValue>
+                  onSubmit={handleCreateFirstStep}
+                  options={{
+                    resolver: yupResolver(firstStepSignUpSchema),
+                    defaultValues: stepFormData,
+                    mode: 'onBlur'
+                  }}
+                >
+                  <FormInput<FirstStepSignUpValue>
+                    name="username"
+                    placeholder="사용자 이름을 입력해 주세요"
+                    className="mb-4"
+                    
+                  />
+                  <FormInput<FirstStepSignUpValue>
+                    name="user_id"
+                    className="mb-4"
+                    placeholder="아이디를 입력해 주세요."
+                  />
+                  <FormInput<FirstStepSignUpValue>
+                    name="password"
+                    className="mb-4"
+                    placeholder="비밀번호를 입력해 주세요."
+                  />
+                  <FormInput<FirstStepSignUpValue>
+                    name="email"
+                    className="mb-4"
+                    placeholder="이메일을 입력해 주세요."
+                  />
+                  {/* warning 문구 제거 */}
+                  { mounted &&
+                  <Button color='blue'>
+                    다음
+                  </Button> 
+                  }
+                </Form>
+              </div>
+            </div>
+          )
+    
+    }
+    
+
+export default FirstStepSignUpForm;
