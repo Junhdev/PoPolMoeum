29886af4ac30f28a2da4134651b44e3b7b1a35ba
이준형 <dlwnsgud261@naver.com>
2023-11-13 23:33:13 +0900

    셀렉션 생성

---
+++ client/src/app/signup/modules/Input/Selection.tsx
@@ -0,0 +1,32 @@
+const universitySelection = [
+    { 
+      id: 1,
+      name: '광운대학교'
+    },
+    { 
+      id: 2,
+      name: '숭실대학교'
+    },
+    { 
+      id: 3,
+      name: '서울과학기술대학교'
+    },
+    { 
+      id: 4,
+      name: '국민대학교'
+    },
+    { 
+      id: 5,
+      name: '중앙대학교'
+    },
+    { 
+      id: 6,
+      name: '서울시립대학교'
+    },
+  ];
+
+
+
+  
+
+  export default universitySelection;
