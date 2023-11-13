87b9a9ff7248eb08599608bf79fd8c440a0ec77e
이준형 <dlwnsgud261@naver.com>
2023-11-13 23:33:57 +0900

    Feat: 셀렉션 생성

---
+++ client/src/app/signup/modules/Input/Selection2.tsx
@@ -0,0 +1,28 @@
+const gradeSelection = [
+    { 
+      id: 1,
+      name: '1학년'
+    },
+    { 
+      id: 2,
+      name: '2학년'
+    },
+    { 
+      id: 3,
+      name: '3학년'
+    },
+    { 
+      id: 4,
+      name: '4학년'
+    },
+    { 
+      id: 5,
+      name: '휴학'
+    },
+    { 
+      id: 6,
+      name: '졸업'
+    },
+  ];
+
+  export default gradeSelection;
